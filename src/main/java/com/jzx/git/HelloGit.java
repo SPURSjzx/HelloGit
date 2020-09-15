2509ad4d703e682874ac8e60112678ad08d092b4
小兆子 <39519593+SPURSjzx@users.noreply.github.com>
2020-09-15 11:38:04 +0800

    Update HelloGit.java

--- src/main/java/com/jzx/git/HelloGit.java
+++ src/main/java/com/jzx/git/HelloGit.java
@@ -14,6 +14,6 @@ public class HelloGit {
         System.out.println("主干添加");
         System.out.println("分支添加");
         System.out.println("远程更新本地");
-
+        System.out.println("在线更新");
     }
 }
