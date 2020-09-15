9e10978fb3b4a0960241c36f8b1d900c6d33d643
SPURSjzx <2329968598@qq.com>
2020-09-15 10:49:03 +0800

    GIT 1

---
+++ src/main/java/com/jzx/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.jzx.git;
+
+/**
+ * @Description:
+ * @title: HelloGit
+ * @Author Administrator
+ * @Date: 2020/9/15 0015 10:42
+ * @Version 1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Git 1");
+    }
+}
